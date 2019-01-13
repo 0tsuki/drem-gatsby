833150808d45fe3f588b91def61974c9a1c7460c
Shinsuke Otsuki <otsuki.shinsuke@gmail.com>
2019-01-13 14:34:47 +0900

    reviewページの本文表示

--- src/templates/reviewPage.js
+++ src/templates/reviewPage.js
@@ -2,12 +2,22 @@ import React from 'react'
 import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
+const bodyStyle = {
+  whiteSpace: 'pre-wrap',
+}
+
 export default ({ data }) => {
   const review = data.review
   return (
     <Layout>
       <h1>{review.title}</h1>
-      <div>Hello review Page</div>
+      TODO: 都道府県, 教習所名, タグ, レーティングの星表示
+      <div>
+        <p>{review.author}</p>
+      </div>
+      <div style={bodyStyle}>
+        <p>{review.body}</p>
+      </div>
     </Layout>
   )
 }
